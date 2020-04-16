a7d796ebac077c7250b48707c44870a21132eff7
MariangelMM <mariangelmoramarquez@gmail.com>
2020-04-16 03:12:45 -0500

    arreglar estilos vistas

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../Style/Sass/style.css';
 
 const Header = () => {
-
     return (
-        <head>
-            <img src={logo} alt="imagen" />
-            <Link>Inicio</Link>
-            <Link>Prestamo</Link>
-            <Link>Red de Emprendedoras</Link>
-            <Link>Mejora tu Negocio</Link>
-        </head>
+        <header >
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+                <img src="https://i.imgur.com/ESXUlFL.png" alt="imagen" />
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse flex-row-reverse " id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <Link class="nav-item nav-link active m-3 " >Inicio <span class="sr-only">(current)</span></Link>
+                        <Link class="nav-item nav-link m-3" >Prestamos</Link>
+                        <Link class="nav-item nav-link m-3" >Red de Emprendedoras</Link>
+                        <Link class="nav-item nav-link m-3" >Mejora tu Negocio</Link>
+                    </div>
+                </div>
+            </nav>
+        </header>
     )
 }
+
+
+
 
 export default Header;
--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
+import Home from "./View/home";
 import GeneralInformation from "./Components/general-information";
 import Requirements from "./Components/upload-requirements";
 import DetailDeposit from "./Components/detail-deposit";
@@ -8,7 +9,9 @@ const Routes = () => {
   return (
     <HashRouter>
       <Switch>
-        <Route exact path="/" component={GeneralInformation} />
+        <Route exact path="/" component={Home} />
+
+        <Route path="/general" component={GeneralInformation} />
         <Route path="/requisitos" component={Requirements} />
         <Route path="/detaildeposit" component={DetailDeposit} />
       </Switch>
