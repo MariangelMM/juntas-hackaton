4653780f36898b5ee69123ec8430e1e004d292a5
MariangelMM <mariangelmoramarquez@gmail.com>
2020-04-17 11:30:01 -0500

    Updates

---
+++ precache-manifest.47aa0de5523a5188326d62b1b9e8fe56.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57d894c1de9285a4d82e648a4c28eb3",
+    "url": "/juntas-hackaton/index.html"
+  },
+  {
+    "revision": "dfdf230d9caae119b8e9",
+    "url": "/juntas-hackaton/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "a8b9727cc3d6522c6fc1",
+    "url": "/juntas-hackaton/static/css/main.0e151e3d.chunk.css"
+  },
+  {
+    "revision": "dfdf230d9caae119b8e9",
+    "url": "/juntas-hackaton/static/js/2.21ac229a.chunk.js"
+  },
+  {
+    "revision": "80d614536119edf4a7ff8645bb597ef3",
+    "url": "/juntas-hackaton/static/js/2.21ac229a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b9727cc3d6522c6fc1",
+    "url": "/juntas-hackaton/static/js/main.a054c13f.chunk.js"
+  },
+  {
+    "revision": "f4d0ab811e6028e836f0",
+    "url": "/juntas-hackaton/static/js/runtime-main.372df7ad.js"
+  },
+  {
+    "revision": "db14b4f91ed475a9889405fe77ecb4c0",
+    "url": "/juntas-hackaton/static/media/DesarrolloPersonal1.db14b4f9.jpg"
+  },
+  {
+    "revision": "bfa99674bc1a5ca9969183288e37ec50",
+    "url": "/juntas-hackaton/static/media/Finanzas1.bfa99674.jpg"
+  },
+  {
+    "revision": "ae0c04daf049f0bb97cfae1186a92823",
+    "url": "/juntas-hackaton/static/media/Finanzas2.ae0c04da.jpg"
+  },
+  {
+    "revision": "1c484ab022aeb0224472700fb7818314",
+    "url": "/juntas-hackaton/static/media/Finanzas3.1c484ab0.jpg"
+  },
+  {
+    "revision": "f4bca87fd0d19e61c27dc96299c75f8c",
+    "url": "/juntas-hackaton/static/media/Metropolis-Medium.f4bca87f.otf"
+  },
+  {
+    "revision": "3218a2890caf3d78c38ddc01840ac473",
+    "url": "/juntas-hackaton/static/media/Ventas2.3218a289.jpg"
+  },
+  {
+    "revision": "e557d5e2c030f34bf1c4e07477de6cbe",
+    "url": "/juntas-hackaton/static/media/image4.e557d5e2.png"
+  },
+  {
+    "revision": "372c772a2cdb0ad795d3b6a3c0029552",
+    "url": "/juntas-hackaton/static/media/img1.372c772a.jpg"
+  },
+  {
+    "revision": "e918e4b099aafd290acc081f2d8589c4",
+    "url": "/juntas-hackaton/static/media/img2.e918e4b0.jpg"
+  },
+  {
+    "revision": "036ab23a729bebc8bb99146742bbc7b8",
+    "url": "/juntas-hackaton/static/media/img3.036ab23a.jpg"
+  }
+]);
